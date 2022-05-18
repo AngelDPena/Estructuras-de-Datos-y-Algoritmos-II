18417edb5d7824ea9e24fc7509f97c38ccce42a6
Blaster2036 <70070167+Blaster2036@users.noreply.github.com>
2022-05-17 23:04:33 -0400

    Crear Header de entrega

---
+++ S3/LaCadenaMasLarga.cpp
@@ -0,0 +1,27 @@
+/*
+    LaCadenaMasLarga.cpp
+    LaCadenaMasLarga
+
+    Enunciado:
+    En una fiesta hay 99 personas. A cada una de ellas se les ha dado un
+    numero entero comprendido entre 1 y 99 acompañado de unas instrucciones.
+    Las instrucciones dicen:
+
+    A) Si su numero es par, divido por 2.
+    B) Si su numero es impar, multiplique por 3 y al resultado añádale 1.
+    C) Repita las instrucciones anteriores con el número que resulte hasta
+       que este sea igual a 4.
+
+    Por tanto, cada invitado obtendrá una cadena de número enteros que termina
+    con el número 4. Por ejemplo, el invitado con el número 3 formará la
+    siguiente cadena: 3 > 10 > 5 > 16 > 8 > 4
+
+    ¿Qué invitado tendrá la cadena más larga?
+
+    Integrantes:
+
+    ID:                 NOMBRE:
+    1099040             Víctor Gómez
+    1103314             Ángel Diego Peña Calderón
+    1091415             Emilio Ariel Santos Mateo
+*/
